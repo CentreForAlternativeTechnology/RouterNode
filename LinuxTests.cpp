21a80e9e0d6ac074f58447896446ad5a74625cd0
Tim Stableford <tim.stableford@gmail.com>
2015-02-24 21:06:50 +0000

    Added test for attribute post

--- LinuxTests.cpp
+++ LinuxTests.cpp
@@ -35,9 +35,9 @@ int main(int argc, char *args[]) {
 	regIdent.attributeNumber = 0x0321;
 	emon.attrIdentAsDataItems(&regIdent, regItems);
 
-	unsigned long fakeReading = 200200;
+	unsigned long fakeDefault = 200200;
 	regItems[3].type = ULONG;
-	regItems[3].item = &fakeReading;
+	regItems[3].item = &fakeDefault;
 
 	regSize = 0;
 	if((regSize = emon.attrSize(ATTR_REGISTER, regItems, 4)) != 25) {
@@ -62,6 +62,48 @@ int main(int argc, char *args[]) {
 		failure++;
 		std::cout << "ERR: attr register output does not match expected output";
 	}
+
+
+	DataItem postItems[4];
+	AttributeIdentifier postIdent;
+	postIdent.groupID = 0x6453;
+	postIdent.attributeID = 0x2321;
+	postIdent.attributeNumber = 0x0321;
+	emon.attrIdentAsDataItems(&postIdent, postItems);
+
+	unsigned long fakeValue = 2052066570;
+	postItems[3].type = ULONG;
+	postItems[3].item = &fakeValue;
+	int postSize = 0;
+	if((postSize = emon.attrSize(ATTR_POST, postItems, 4)) != 25) {
+		failure++;
+		std::cout << "ERR: wrong packet size for attribute register " << postSize << "\n";
+	} else {
+		passCount++;
+	}
+
+	unsigned char postBuffer[postSize];
+	if(postSize != emon.attrBuilder(ATTR_POST, postItems, 4, postBuffer)) {
+		std::cout << "ERR: attr post estimated size does not equal actual size expected " << postSize << "\n";
+		failure++;
+	} else {
+		passCount++;
+	}
+
+	unsigned char postCmp[] = { 0x0, 0x5, 0x15, 0x0, 0x4, 0x0, 0x0, 0x4, 0x53, 0x64, 0x4, 0x21, 0x23, 0x4,
+				0x21, 0x3, 0x9, 0xa, 0xd, 0x50, 0x7a, 0x0, 0x0, 0x0, 0x0 };
+	if(memcmp(postBuffer, postCmp, postSize) == 0) {
+		passCount++;
+	} else {
+		failure++;
+		std::cout << "ERR: attr post output does not match expected output";
+	}
+
+	for(int i=0; i<postSize; i++) {
+		printf("0x%x ", postBuffer[i]);
+	}
+	std::cout << "\n";
+	
 	
 	std::cout << passCount << " passes of " << (passCount + failure) << "\n";
 	
